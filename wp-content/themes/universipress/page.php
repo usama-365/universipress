6cdaf4ac1d0ed8fda70dd4ed2c8dd7488164d66f
usama-365 <usama-365@protonmail.com>
2022-08-26 19:36:17 +0500

    Made the breadcrumb of current page location and parent page functional for going to parent page

--- wp-content/themes/universipress/page.php
+++ wp-content/themes/universipress/page.php
@@ -4,7 +4,8 @@ while (have_posts()) {
     the_post();
     ?>
     <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>"></div>
+        <div class="page-banner__bg-image"
+             style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>"></div>
         <div class="page-banner__content container container--narrow">
             <h1 class="page-banner__title"><?php the_title() ?></h1>
             <div class="page-banner__intro">
@@ -14,12 +15,19 @@ while (have_posts()) {
     </div>
 
     <div class="container container--narrow page-section">
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p>
-                <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About
-                    Us</a> <span class="metabox__main">Our History</span>
-            </p>
-        </div>
+        <?php
+        $parent_id = wp_get_post_parent_id(get_the_ID());
+        if ($parent_id) {
+            ?>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <a class="metabox__blog-home-link" href="<?php echo get_the_permalink($parent_id) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to
+                        <?php echo get_the_title($parent_id) ?></a> <span class="metabox__main"><?php the_title() ?></span>
+                </p>
+            </div>
+            <?php
+        }
+        ?>
 
         <!--
         <div class="page-links">
