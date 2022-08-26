25f4be1eb5bb651d4851f6d9950818d119a3c8ae
usama-365 <usama-365@protonmail.com>
2022-08-26 18:14:00 +0500

    Integrated front-end template for the index page

--- wp-content/themes/universipress/functions.php
+++ wp-content/themes/universipress/functions.php
@@ -1,7 +1,11 @@
 <?php
 
-function load_css_files() {
-    wp_enqueue_style('universipress_main_css', get_stylesheet_uri());
+function load_custom_files() {
+    wp_enqueue_style("universipress_main_css", get_theme_file_uri("build/style-index.css"));
+    wp_enqueue_style("universipress_extra_css", get_theme_file_uri("build/index.css"));
+    wp_enqueue_style("font_awesome", "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+    wp_enqueue_style("google_fonts", "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
+    wp_enqueue_script("universipress_main_js", get_theme_file_uri("build/index.js"), array('jquery'), '1.0', true);
 }
 
-add_action('wp_enqueue_scripts', 'load_css_files');
+add_action('wp_enqueue_scripts', 'load_custom_files');
