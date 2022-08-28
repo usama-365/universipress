c7c6a641ba6c55625fe4709d6e7dcf1b29f34e95
usama-365 <usama-365@protonmail.com>
2022-08-28 15:27:04 +0500

    Added pagination on index.php and modified style of single.php

--- wp-content/themes/universipress/index.php
+++ wp-content/themes/universipress/index.php
@@ -14,9 +14,11 @@
             the_post();
             ?>
             <div class="post-item">
-                <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
+                <h2 class="headline headline--medium headline--post-title"><a
+                            href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
                 <div class="metabox">
-                    <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('M d, Y'); ?> in <?php the_category(', '); ?></p>
+                    <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('M d, Y'); ?>
+                        in <?php the_category(', '); ?></p>
                 </div>
                 <div class="generic-content">
                     <?php the_excerpt(); ?>
@@ -24,6 +26,8 @@
                 <a class="btn btn--blue" href="<?php the_permalink(); ?>"><p>Continue reading &raquo;</p></a>
             </div>
             <?php
-        } ?>
+        }
+        echo paginate_links();
+        ?>
     </div>
 <?php get_footer(); ?>
